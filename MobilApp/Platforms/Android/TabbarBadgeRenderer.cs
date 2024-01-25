0164bdab5efda726dcd6a984daa03b9acf907024
farkaslevente <farkas.levente2004@gmail.com>
2024-01-25 09:18:39 +0100

    V3: TabBar Badges in progress

---
+++ MobilApp/Platforms/Android/TabbarBadgeRenderer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui.Controls.Handlers.Compatibility;
+using Microsoft.Maui.Controls.Platform.Compatibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilApp_Szakdolgozat.Platforms.Android
+{
+    public class TabbarBadgeRenderer: ShellRenderer
+    {
+        public TabbarBadgeRenderer()
+        {
+            
+        }
+
+        protected override IShellBottomNavViewAppearanceTracker CreateBottomNavViewAppearanceTracker(ShellItem shellItem)
+        {
+            return base.CreateBottomNavViewAppearanceTracker(shellItem);
+        }
+    }
+}
