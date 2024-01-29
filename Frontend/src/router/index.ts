6b517871048c92ef45636b13e1ccc5c8a75da832
DanielParraghy <parraghydaniel@gmail.com>
2024-01-29 12:15:34 +0100

    RegistrationView + routes

--- Frontend/src/router/index.ts
+++ Frontend/src/router/index.ts
@@ -1,10 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router' 
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import RegistrationView from '../views/RegistrationView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: HomeView }
+    { path: '/bejelentkezes', component: LoginView }
+    { path: '/registration', component: RegistrationView }
   ]
 })
 
