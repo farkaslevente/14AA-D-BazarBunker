d31cf60b36a6e546c8ed00638ad554025e087d60
DanielParraghy <parraghydaniel@gmail.com>
2024-02-01 09:35:38 +0100

    továbbra se jó

--- Frontend/src/router/index.ts
+++ Frontend/src/router/index.ts
@@ -6,10 +6,10 @@ import RegistrationView from '../views/RegistrationView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: HomeView }
-    { path: '/bejelentkezes', component: LoginView }
+    { path: '/', component: HomeView },
+    { path: '/bejelentkezes', component: LoginView },
     { path: '/registration', component: RegistrationView }
   ]
-})
+});
 
 export default router
