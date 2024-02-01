de0e33ddcf552229d928c10f157b08ccc301a515
Farkas Levente <farkas.levente2004@gmail.com>
2024-02-01 12:40:15 +0100

    V13: ProfilePictureCatalog Page layout is done, lacks data, data collection is in progress

--- MobilApp/Views/PPCatalogPage.xaml.cs
+++ MobilApp/Views/PPCatalogPage.xaml.cs
@@ -1,9 +1,22 @@
+
 namespace MobilApp_Szakdolgozat.Views;
 
 public partial class PPCatalogPage : ContentPage
 {
+	public static List<string> ppList;
 	public PPCatalogPage()
 	{
 		InitializeComponent();
+		Init();
 	}
+
+    private void Init()
+    {
+		ppList = new List<string>();
+		for (int i = 0; i < 50; i++)
+		{
+			ppList.Add(i.ToString());
+		}
+		ppCW.ItemsSource = ppList;
+    }
 }
