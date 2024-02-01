501e2b8518cc01bdda8710cd0b911f941e328e09
Farkas Levente <farkas.levente2004@gmail.com>
2024-02-01 11:11:04 +0100

    V11: Profile Name can be changed, Profile picture library is in progress

--- MobilApp/Views/ProfilePage.xaml.cs
+++ MobilApp/Views/ProfilePage.xaml.cs
@@ -5,5 +5,22 @@ public partial class ProfilePage : ContentPage
 	public ProfilePage()
 	{
 		InitializeComponent();
-	}
+        NameChangeBTN.IsVisible = true;
+        NameSaveBTN.IsVisible = false;
+
+    }
+
+    private void NameChangeBTN_Clicked(object sender, EventArgs e)
+    {
+		NameChangeBTN.IsVisible = false;
+        NameSaveBTN.IsVisible = true;
+        NameEntry.Text = "";
+    }
+
+    private void NameSaveBTN_Clicked(object sender, EventArgs e)
+    {
+        NameChangeBTN.IsVisible = true;
+        NameSaveBTN.IsVisible = false;
+        NameLB.Text = NameEntry.Text;
+    }
 }
