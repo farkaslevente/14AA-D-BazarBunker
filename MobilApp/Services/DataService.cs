b048964d39006a2dc12b80d01623ab182523b2ff
farkaslevente <FARKAS.LEVENTE2004@GMAIL.COM>
2024-02-03 15:08:27 +0100

    V14.3: DataService mainly completed, needs routes

--- MobilApp/Services/DataService.cs
+++ MobilApp/Services/DataService.cs
@@ -1,5 +1,6 @@
 ﻿using MobilApp_Szakdolgozat.Models;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,6 +35,65 @@ namespace MobilApp_Szakdolgozat.Services
                 var result = await client.GetStringAsync(uri);
 
                 return JsonConvert.DeserializeObject<List<PictureCatalogModel>>(result);
+            }
+        }
+
+        public static async Task<RegisterModel> register(RegisterModel user) 
+        {
+            RegisterModel errorRegister = new RegisterModel();
+
+            string jsonData = JsonConvert.SerializeObject(user);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.PostAsync(url + "/register", content); //Majd meg kell adni a végleges backend elérést
+
+            // Debug.WriteLine(response.StatusCode);
+            string result = await response.Content.ReadAsStringAsync();
+
+            if ((int)response.StatusCode == 400) 
+            {
+                //hiba
+                dynamic errorObj = JsonConvert.DeserializeObject<dynamic>(result);
+                foreach (JProperty variable in errorObj) 
+                {
+                    if (variable.Name == "name")
+                    {
+                        errorRegister.name = variable.Value[0].ToString();
+                    }
+                    if (variable.Name == "email")
+                    {
+                        errorRegister.email = variable.Value[0].ToString();
+                    }
+                    if (variable.Name == "password")
+                    {
+                        errorRegister.password = variable.Value[0].ToString();
+                    }
+                    if (variable.Name == "confirm_password")
+                    {
+                        errorRegister.confirm_password = variable.Value[0].ToString();
+                    }
+                }
+            }
+            return errorRegister;
+        }
+
+        public static async Task<string> login(string email, string password) 
+        {
+            string jsonData = JsonConvert.SerializeObject(new { email = email, password = password });
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.PostAsync(url + "/login", content); //Majd meg kell adni a végleges backend elérést
+
+            string result = await response.Content.ReadAsStringAsync();
+
+            if ((int)response.StatusCode == 401)
+            {
+                return "Helytelen bejelentkezés!";
+            }
+            else
+            {
+                await SecureStorage.SetAsync("user", result);
+                return null;
             }
         }
 
