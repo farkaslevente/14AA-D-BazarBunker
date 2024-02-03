564beb10343923d01ffe1a7e6c233d51703b2f43
farkaslevente <FARKAS.LEVENTE2004@GMAIL.COM>
2024-02-03 13:16:25 +0100

    V14: PPCatalogViewModel v1 done, still working on it

---
+++ MobilApp/Models/PictureCatalogModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilApp_Szakdolgozat.Models
+{
+    public class PictureCatalogModel
+    {
+        public int Id { get; set; }
+        public int Url { get; set; }
+    }
+}
--- MobilApp/ViewModels/PPCatalogViewModel.cs
+++ MobilApp/ViewModels/PPCatalogViewModel.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using MobilApp_Szakdolgozat.Models;
+using MobilApp_Szakdolgozat.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MobilApp_Szakdolgozat.ViewModels
 {
-    class PPCatalogViewModel
+    public class PPCatalogViewModel: BindableObject
     {
+        public ObservableCollection<PictureCatalogModel> pictures { get; set; }
+        public PictureCatalogModel selectedPicture { get; set; }
+        public ICommand detailsCommand { get; set; }
+        
+        public PPCatalogViewModel() 
+        { 
+            pictures = new ObservableCollection<PictureCatalogModel>();
+            getAllPictures();
+            detailsCommand = new Command(async () => {
+                if (selectedPicture == null) return;
+                await Shell.Current.GoToAsync("details",
+                    new Dictionary<string, object> { { "picture", selectedPicture } });
+
+                selectedPicture = null;
+                OnPropertyChanged(nameof(selectedPicture));
+            });
+        }
+
+        private async void getAllPictures()
+        {
+            IEnumerable<PictureCatalogModel> list = await DataService.getAllPictures();
+            list.ToList().ForEach(p => pictures.Add(p));
+        }
     }
 }
