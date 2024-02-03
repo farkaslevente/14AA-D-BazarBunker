8b72c62c525a98cd8c8343bd5653d358f76c48f9
farkaslevente <FARKAS.LEVENTE2004@GMAIL.COM>
2024-02-03 14:02:11 +0100

    V14.2: DataService created, getAllProfiles() and getAllPictures() finished, waiting for backend route

--- MobilApp/Services/DataService.cs
+++ MobilApp/Services/DataService.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using MobilApp_Szakdolgozat.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 
 namespace MobilApp_Szakdolgozat.Services
 {
     public class DataService
     {
+        static string url = "https://bgs.jedlik.eu";
+
+        public static async Task<IEnumerable<ProfileModel>> getAllProfiles()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(url);
+                var uri = "/users"; //Majd meg kell adni a végleges backend elérést
+                var result = await client.GetStringAsync(uri);
+
+                return JsonConvert.DeserializeObject<List<ProfileModel>>(result);
+            }
+        }
+
+        public static async Task<IEnumerable<PictureCatalogModel>> getAllPictures() 
+        {
+            using (var client = new HttpClient()) 
+            { 
+                client.BaseAddress = new Uri(url);
+                var uri = "/pictures"; //Majd meg kell adni a végleges backend elérést
+                var result = await client.GetStringAsync(uri);
+
+                return JsonConvert.DeserializeObject<List<PictureCatalogModel>>(result);
+            }
+        }
+
+
     }
 }
