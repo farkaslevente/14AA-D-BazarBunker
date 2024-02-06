20b489794b14ce355780b011185a4e988a0d4026
Farkas Levente <farkas.levente2004@gmail.com>
2024-02-06 09:17:58 +0100

    V16: PictureCatalogModel completed, SQL commands implemented

--- MobilApp/Models/PictureCatalogModel.cs
+++ MobilApp/Models/PictureCatalogModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +10,41 @@ namespace MobilApp_Szakdolgozat.Models
 {
     public class PictureCatalogModel
     {
-        public int Id { get; set; }
-        public int Url { get; set; }
+        public int? Id { get; set; }
+        public string Url { get; set; }
+
+        public PictureCatalogModel(MySqlDataReader reader) 
+        {
+            this.Id = (int?)Convert.ToInt32(reader["id"]);
+            this.Url = reader["url"].ToString();
+        }
+
+        public PictureCatalogModel(int? Id, string Url) 
+        {
+            this.Id = Id;
+            this.Url = Url;
+        }
+
+        public static List<PictureCatalogModel> select() 
+        {
+            var list = new List<PictureCatalogModel>();
+            using (var con = new MySqlConnection(ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString)) 
+            {
+                con.Open();
+                var sql = "SELECT * FROM kepek";
+                using (var cmd = new MySqlCommand(sql, con))
+                {
+                    using (var reader = cmd.ExecuteReader()) 
+                    { 
+                        while (reader.Read()) 
+                        {
+                            list.Add(new PictureCatalogModel(reader));
+                        }
+                    }
+                }
+                con.Close();
+            }
+            return list;
+        }
     }
 }
