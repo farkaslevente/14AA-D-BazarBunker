7c21165efc93529e022108a39a2c15c0f05fd5f7
markobence <62424891+markobence@users.noreply.github.com>
2024-02-13 09:04:03 +0100

    Merging (#21)
    * merged
    
    * backend only

--- MobilApp/Views/ConversationsPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace MobilApp_Szakdolgozat.Views;
-
-public partial class ConversationsPage : ContentPage
-{
-	public ConversationsPage()
-	{
-		InitializeComponent();
-	}
-}
--- MobilApp/Views/ForgottenPwdPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace MobilApp_Szakdolgozat.Views;
-
-public partial class ForgottenPwdPage : ContentPage
-{
-	public ForgottenPwdPage()
-	{
-		InitializeComponent();
-	}
-}
