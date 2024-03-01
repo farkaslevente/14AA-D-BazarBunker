95dc2e08b29cca9c6c267d0d5bf634f4c6e08f25
DanielParraghy <parraghydaniel@gmail.com>
2024-03-01 10:31:18 +0100

    primevue

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -7,6 +7,8 @@ import router from './router'
 import 'bootstrap'
 import './assets/scss/style.scss';
 
+import 'primevue/resources/themes/aura-light-green/theme.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
