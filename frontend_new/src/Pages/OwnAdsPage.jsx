677c4132a84fc2dc4a0be063aa056840adb53532
ParraghyDaniel <parraghy.daniel.jozsef@students.jedlik.eu>
2024-03-10 08:26:21 +0100

    Owncards for Owncardpage (same as regular cards)

--- frontend_new/src/Pages/OwnAdsPage.jsx
+++ frontend_new/src/Pages/OwnAdsPage.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { OwnCard } from '../Components/OwnCard/OwnCard'
 
 export const OwnAdsPage = () => {
-  return (
-    <div>OwnAdsPage</div>
-  )
+    return (
+        <div className='alladspage'>
+            <div className="alladspage-title">
+                <h1>Saját hirdetések</h1>
+            </div>
+            <div className="alladspage-content">
+                <div className="cards">
+                    <OwnCard/>
+                    <OwnCard/>
+                    <OwnCard/>
+                    <OwnCard/>
+                    <OwnCard/>
+                </div>
+            </div>
+        </div>
+    )
 }
