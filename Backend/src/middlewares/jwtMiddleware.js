b0309332440682c7a43da9cebbc0667bd455ee11
markobence <marko.bence.mendbajr@students.jedlik.eu>
2024-03-12 11:32:36 +0100

    authorizing cont.

--- Backend/src/middlewares/jwtMiddleware.js
+++ Backend/src/middlewares/jwtMiddleware.js
@@ -2,30 +2,39 @@ const jwt = require('jsonwebtoken')
 const { secret } = require('../config/auth.config')
 
 verifyToken = (req, res, next) => {
-    let token = req.session.token;
-  
-    if (!token) {
+
+  const token = req.headers['authorization'];
+  if (!token || !token.startsWith('Bearer ')) {
+    return res.status(403).send({ message: "Invalid token or not a Bearer token" });
+  }
+  const tokenOnly = token.slice(7, token.length);
+  console.log(tokenOnly)
+
+    if (!tokenOnly) {
       return res.status(403).send({ message: "Invalid token" });
     }
   
-    jwt.verify(token,
-                secret,
-              (err, decoded) => {
-                if (err) {
-                  return res.status(401).send({
-                    message: "Unauthorized",
-                  });
-                }
-                req.userId = decoded.id;
-                next();
-              });
+    jwt.verify(tokenOnly,
+      secret,
+      (err, decoded) => {
+        if (err) {
+          // Handle the case when the token is invalid or expired
+          if (err.name === 'TokenExpiredError') {
+            return res.status(401).send({ message: "Token expired" });
+          } else {
+            return res.status(401).send({ message: "Unauthorized" });
+          }
+        }
+        req.userId = decoded.id;
+        next();
+      });
   };
 
  function accessToken (payload) {
     const token = jwt.sign(
         payload,
         secret,
-        { expiresIn: '10s'});
+        { expiresIn: '1d'});
     return token;
  } 
 
