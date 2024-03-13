741fb21344c65d5edcba07bc2543468f7c09e140
ParraghyDaniel <parraghy.daniel.jozsef@students.jedlik.eu>
2024-03-13 18:59:15 +0100

    still shit

--- frontend_new/src/Pages/RegisterPage.jsx
+++ frontend_new/src/Pages/RegisterPage.jsx
@@ -5,63 +5,7 @@ import { FormInput } from '../Components/FormInput/FormInput';
 import axios from 'axios';
 
 export const RegisterPage = () => {
-//     const [values, setValues] = useState({
-//         username: "",
-//         email: "",
-//         password: "",
-//         passwordagain: ""
-//     });
-//     const navigate = useNavigate()
-
-//     const inputs = [
-//         {
-//             id: 1,
-//             name:"username",
-//             type: "text",
-//             placeholder: "Felhasználónév",
-//             errorMessage: "A felhasználónév 3-16 karakter hosszúnak kell lenni, és nem tartalmazhat speciális karaktereket!",
-//             label: "Felhasználónév",
-//             pattern: "^[A-Za-z0-9]{3,16}$",
-//             required: true,
-//         },
-//         {
-//             id: 2,
-//             name:"email",
-//             type: "email",
-//             placeholder: "Email",
-//             label: "Email",
-//             errorMessage: "Az email nem megfelelő formátumú!",
-//             required: true
-//         },
-//         {
-//             id: 3,
-//             name: "password",
-//             type: "password",
-//             placeholder: "Jelszó",
-//             label: "Jelszó",
-//             errorMessage: "A jelszónak 8-30 karakter hosszúnak kell lenni és tartalmaznia kell legalább egyet-egyet az alábbiak közül: Betű, Szám, Speciális karakter!",
-//             pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,20}$`,
-//             required: true
-//         },
-//         {
-//             id: 4,
-//             name: "passwordagain",
-//             type: "password",
-//             placeholder: "Jelszó újra",
-//             label: "Jelszó újra",
-//             errorMessage: "A jelszó nem egyezik a másik jelszóval!",
-//             pattern: values.password,
-//             required: true
-//         }
-//     ];
-
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//     };
-
-//     const onChange = (e) => {
-//         setValues({...values, [e.target.name]: e.target.value});
-//     }
+    const navigate = useNavigate();
 
     const [values, setValues] = useState({
         name: '',
@@ -71,7 +15,9 @@ export const RegisterPage = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        axios.post('')
+        axios.post('http://localhost:9000/register', values)
+            .then(res => console.log(res))
+            .then(err => console.log(err));
     }
 
     return (
@@ -81,20 +27,81 @@ export const RegisterPage = () => {
                 <form onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label htmlFor="name"><strong>Felhasználónév</strong></label>
-                        <input type="text" placeholder='Felhasználónév'name='name' className='form-control rounded-0' onChange={e => setValues({...values, name: e.target.value})}/>
+                        <input type="text" placeholder='Felhasználónév' name='name' className='form-control rounded-0'
+                            onChange={e => setValues({ ...values, name: e.target.value })} />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="email"><strong>Email</strong></label>
-                        <input type="email" placeholder='Email'name='email' className='form-control rounded-0' onChange={e => setValues({...values, email: e.target.value})}/>
+                        <input type="email" placeholder='Email' name='email' className='form-control rounded-0'
+                            onChange={e => setValues({ ...values, email: e.target.value })} />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="password"><strong>Jelszó</strong></label>
-                        <input type="password" placeholder='Jelszó'name='password' className='form-control rounded-0' onChange={e => setValues({...values, password: e.target.value})}/>
+                        <input type="password" placeholder='Jelszó' name='password' className='form-control rounded-0'
+                            onChange={e => setValues({ ...values, password: e.target.value })} />
                     </div>
                     <button type='submit' className='btn btn-success w-100 rounded-0'>Regisztráció</button>
                 </form>
             </div>
         </div>
+
+        //     const [values, setValues] = useState({
+        //         username: "",
+        //         email: "",
+        //         password: "",
+        //         passwordagain: ""
+        //     });
+        //     const navigate = useNavigate()
+
+        //     const inputs = [
+        //         {
+        //             id: 1,
+        //             name:"username",
+        //             type: "text",
+        //             placeholder: "Felhasználónév",
+        //             errorMessage: "A felhasználónév 3-16 karakter hosszúnak kell lenni, és nem tartalmazhat speciális karaktereket!",
+        //             label: "Felhasználónév",
+        //             pattern: "^[A-Za-z0-9]{3,16}$",
+        //             required: true,
+        //         },
+        //         {
+        //             id: 2,
+        //             name:"email",
+        //             type: "email",
+        //             placeholder: "Email",
+        //             label: "Email",
+        //             errorMessage: "Az email nem megfelelő formátumú!",
+        //             required: true
+        //         },
+        //         {
+        //             id: 3,
+        //             name: "password",
+        //             type: "password",
+        //             placeholder: "Jelszó",
+        //             label: "Jelszó",
+        //             errorMessage: "A jelszónak 8-30 karakter hosszúnak kell lenni és tartalmaznia kell legalább egyet-egyet az alábbiak közül: Betű, Szám, Speciális karakter!",
+        //             pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,20}$`,
+        //             required: true
+        //         },
+        //         {
+        //             id: 4,
+        //             name: "passwordagain",
+        //             type: "password",
+        //             placeholder: "Jelszó újra",
+        //             label: "Jelszó újra",
+        //             errorMessage: "A jelszó nem egyezik a másik jelszóval!",
+        //             pattern: values.password,
+        //             required: true
+        //         }
+        //     ];
+
+        //     const handleSubmit = (e) => {
+        //         e.preventDefault();
+        //     };
+
+        //     const onChange = (e) => {
+        //         setValues({...values, [e.target.name]: e.target.value});
+        //     }
 
         // <div className="registerpage">
         //     <div className="register-container">
@@ -109,19 +116,19 @@ export const RegisterPage = () => {
         // </div>
 
 
-    // <div className='registerpage'>
-    //   <div className="register-container">
-    //     <h1>Regisztráció</h1>
-    //     <div className="register-fields">
-    //       <input type="text" placeholder='Felhasználónév' />
-    //       <input type="email" placeholder='Email' />
-    //       <input type="password" placeholder='Jelszó' />
-    //       <input type="password" placeholder='Jelszó újra' />
-    //     </div>
-    //     <div className="">
-    //       <button onClick={() => navigate('/adatregisztracio')}>Tovább</button>
-    //     </div>
-    //   </div>
-    // </div>
-  )
-    }
+        // <div className='registerpage'>
+        //   <div className="register-container">
+        //     <h1>Regisztráció</h1>
+        //     <div className="register-fields">
+        //       <input type="text" placeholder='Felhasználónév' />
+        //       <input type="email" placeholder='Email' />
+        //       <input type="password" placeholder='Jelszó' />
+        //       <input type="password" placeholder='Jelszó újra' />
+        //     </div>
+        //     <div className="">
+        //       <button onClick={() => navigate('/adatregisztracio')}>Tovább</button>
+        //     </div>
+        //   </div>
+        // </div>
+    )
+}
