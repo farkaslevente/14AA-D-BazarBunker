1efcdf56042cf607b9ad29e33123ad728be3e4e2
ParraghyDaniel <parraghy.daniel.jozsef@students.jedlik.eu>
2024-03-17 22:05:42 +0100

    installed react bootstrap tried carousel -> could work did a bit of restyling

--- frontend_new/src/Components/OwnCard/OwnCard.jsx
+++ frontend_new/src/Components/OwnCard/OwnCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './OwnCard.css'
 import { useNavigate } from 'react-router-dom'
+import Carousel from 'react-bootstrap/Carousel';
 
 export const OwnCard = () => {
     const navigate = useNavigate()
--- frontend_new/src/Pages/OwnAdsPage.jsx
+++ frontend_new/src/Pages/OwnAdsPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { OwnCard } from '../Components/OwnCard/OwnCard'
+import './CSS/OwnAdPage.css'
 
 export const OwnAdsPage = () => {
     return (
-        <div className='alladspage'>
-            <div className="alladspage-title">
+        <div className='ownadspage'>
+            <div className="ownadspage-title">
                 <h1>Saját hirdetések</h1>
             </div>
-            <div className="alladspage-content">
+            <div className="ownadspage-content">
                 <div className="cards">
                     <OwnCard/>
                     <OwnCard/>
