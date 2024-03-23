2106fbacd657ebb9c29ea4b319c7d7888b1bd72c
ParraghyDaniel <parraghy.daniel.jozsef@students.jedlik.eu>
2024-03-23 18:19:56 +0100

    little extra

--- frontend_new/src/Components/Footer/Footer.jsx
+++ frontend_new/src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 export const Footer = () => {
   return (
     <div className='footer'>
-        <p>{'\u00a9'} 2024 - PD, MB, FL</p>
+        <p>{'\u00a9'} 2024 - <a href="https://github.com/danielparraghy">PD</a>, <a href="https://github.com/markobence">MB</a>, <a href="https://github.com/farkaslevente">FL</a></p>
     </div>
   )
 }
