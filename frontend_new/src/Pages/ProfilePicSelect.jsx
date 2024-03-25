083e17156cf35e56d08736666bb898ab7d91850d
ParraghyDaniel <parraghy.daniel.jozsef@students.jedlik.eu>
2024-03-25 11:21:24 +0100

    pp selectpage

---
+++ frontend_new/src/Pages/ProfilePicSelect.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './CSS/ProfilePicSelect.css'
+
+export const ProfilePicSelect = () => {
+  return (
+    <div>Will contain the possible pictures for profile pictures</div>
+  )
+}
