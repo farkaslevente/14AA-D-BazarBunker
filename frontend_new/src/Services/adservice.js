17afa3cce1bf699158cbc6a0f14cfd9defa0ef6d
ParraghyDaniel <parraghy.daniel.jozsef@students.jedlik.eu>
2024-03-26 11:08:51 +0100

    axios

--- frontend_new/src/Services/adservice.js
+++ frontend_new/src/Services/adservice.js
@@ -2,13 +2,31 @@ import Axios from './dataservice.js'
 
 const adservice = {
     getAllAds() {
-        return Axios.get('ads')
+        return Axios.get('/ads')
             .then(resp => {
                 return resp.data;
             })
             .catch(err => {
                 return Promise.reject(err);
             });
+    },
+    getCounties() {
+        return Axios.get('/counties')
+            .then(resp => {
+                return resp.data;
+            })
+            .catch(err => {
+                return Promise.reject(err);
+            })
+    },
+    getSettlements() {
+        return Axios.get('/settlements')
+            .then(resp => {
+                return resp.data;
+            })
+            .catch(err => {
+                return Promise.reject(err);
+            })
     },
 };
 
