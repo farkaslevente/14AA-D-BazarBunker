37e90e0c4853b1594cc05b06142482704589f8b4
DanielParraghy <parraghydaniel@gmail.com>
2024-04-10 14:10:17 +0200

    LoginPage is responsive

--- frontend_new/src/Pages/LoginPage.jsx
+++ frontend_new/src/Pages/LoginPage.jsx
@@ -1,21 +1,20 @@
-import React, { useState } from 'react'
-import './CSS/LoginPage.css'
-import { useNavigate } from 'react-router-dom'
-import userservice from '../Services/userservice'
+import React, { useState } from 'react';
+import './CSS/LoginPage.css';
+import { useNavigate } from 'react-router-dom';
+import userservice from '../Services/userservice';
 import { jwtDecode } from "jwt-decode";
 
 export const LoginPage = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     const [error, setError] = useState(null);
-
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const resp = await userservice.login({ email: email, password: password })
+            const resp = await userservice.login({ email: email, password: password });
             const token = resp.data.token;
             const decoded = jwtDecode(token);
             const authEmail = decoded.payload.email;
@@ -44,7 +43,7 @@ export const LoginPage = () => {
         } catch (error) {
             setError("Hibás email cím vagy jelszó!");
         }
-    }
+    };
 
     return (
         <div className='loginpage'>
@@ -59,13 +58,13 @@ export const LoginPage = () => {
                         <input className='rounded' type="password" id='password' autoComplete='off' required placeholder='Jelszó' value={password} onChange={(e) => setPassword(e.target.value)} />
                     </div>
                     {error && <div className="errors">{error}</div>}
-                    <div>
+                    <div style={{textAlign: 'center'}}>
                         <button className='rounded' type='submit'>Bejelentkezés</button>
-                        <p className="login-signup">Még nincs fiókja? <a style={{ textDecoration: 'none' }} href='/regisztracio'>Hozzon létre egyet!</a></p>
-                        <p className="login-signup"><a style={{ textDecoration: 'none' }} href='/elfelejtettjelszo'>Elfelejtett jelszó?</a></p>
+                        <p className="login-signup">Még nincs fiókja? <a href='/regisztracio'>Hozzon létre egyet!</a></p>
+                        <p className="login-signup"><a href='/elfelejtettjelszo'>Elfelejtett jelszó?</a></p>
                     </div>
                 </form>
             </div>
         </div>
-    )
-}
+    );
+};
