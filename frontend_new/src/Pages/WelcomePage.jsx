4527e896a20c7370518feab923386e13055ea0de
DanielParraghy <parraghydaniel@gmail.com>
2024-04-10 14:04:04 +0200

    WelcomePage is responsive

--- frontend_new/src/Pages/WelcomePage.jsx
+++ frontend_new/src/Pages/WelcomePage.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import './CSS/WelcomePage.css'
+import React from 'react';
+import './CSS/WelcomePage.css';
 
 export const WelcomePage = () => {
   return (
     <div className='welcomepage'>
       <div className="container">
-        <h1 className='title'>Végeztél az iskolával? - Vagy éppen most kezded?</h1>
+        <h1 className='title'>Végeztél az iskolával? <br/>- <br/>Vagy éppen most kezded?</h1>
         <div className="description">
           <p>A <b><i>BAZÁRBUNKER</i>&trade;</b> azért jött létre, hogy mindenki számára megkönnyítse és olcsóbbá tegye az iskolát.</p>
           <p>Ha nem bánod az esetleges használt cuccokat, akkor <b>ITT</b> a helyed, nézz körül, és spórolj!</p>
-          <p>Legyen szó színes ceruza készletről, köteleő olvasmányokról, tankönyvekről, akár íróasztalról is, itt könnyedén megtalálhatod!</p><br /><br />
+          <p>Legyen szó színes ceruza készletről, kötelező olvasmányokról, tankönyvekről, akár íróasztalról is, itt könnyedén megtalálhatod!</p>
         </div>
-        <div className="">
-          <strong>
-            <p>Problémába ütközött? <a href='/support'>Tudjon meg többet!</a></p>
-          </strong>
+        <div className="support-link">
+          <p><strong>Problémába ütközött? <a href='/support'>Tudjon meg többet!</a></strong></p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
