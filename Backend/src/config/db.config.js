a35a90379736d4011903756617627e9816dc72d8
markobence <marko.bence.mendbajr@students.jedlik.eu>
2024-04-17 19:33:15 +0200

    final ig

--- Backend/src/config/db.config.js
+++ Backend/src/config/db.config.js
@@ -1,12 +1,13 @@
 const { config } = require("dotenv");
 config();
+const {DB_user, DB_host, DB_pwd, DB_name} = process.env
 
 const conf = {
     db: {
-        host: process.env.DB_local_host,
-        user: process.env.DB_local_user,
-        password: process.env.DB_local_pwd, 
-        database: process.env.DB_name,
+        host: DB_host,
+        user: DB_user,
+        password: DB_pwd, 
+        database: DB_name,
         connectTimeout: 60000
     }
 };
